c31246b9f2f7584fb7f7541b9cf0bd34a43cb25c
Jiao-man-22 <2935996123@qq.com>
2020-08-18 21:41:21 +0800

    boot项目创建

---
+++ springboot/src/main/java/com/jiao/Springboot.java
@@ -0,0 +1,12 @@
+package com.jiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Springboot{
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot.class,args);
+    }
+}
